0d1ac7d53f1634b82a499e79cc47e5cf661ca09c
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-13 14:30:54 -0300

    Testando um métodp get

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const app = express();
 
 app.use(express.json())
 
+app.get('/api/ping', (request, response) => {
+    response.send({
+        message: 'pong'
+    }
+    )
+});
+
 //subir o servidor na porta 8000
 app.listen(8000, ()=>{
     console.log("Servidor funcionando na porta 8000...")
