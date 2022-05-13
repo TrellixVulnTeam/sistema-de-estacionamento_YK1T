81878233b63436e189335b435fd429c9d35e9ad3
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-13 14:43:33 -0300

    Metodo get de Veiculos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //importando o express
 import express from 'express'; 
+import { get } from 'express/lib/response';
 const app = express();
 
 app.use(express.json())
@@ -10,6 +11,25 @@ app.get('/api/ping', (request, response) => {
     }
     )
 });
+
+//ENDPOINTS VEHICLES
+app.get('/api/vehicles', (request, response) =>{
+    response.send([
+        {
+            id: 1, 
+            name: 'Onix 1.4', 
+            owner: 'Marcus Vinicius',
+            type: 'car'
+        }, 
+        {
+            id: 2, 
+            name: 'Cobalt Cinza', 
+            owner: 'Ana Carla',
+            type: 'car'
+        },
+    ])
+})
+
 
 //subir o servidor na porta 8000
 app.listen(8000, ()=>{
